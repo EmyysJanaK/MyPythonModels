a303807e39c3a99df9dd60d1c2ea46866a99adaf
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2024-01-27 13:45:20 +0530

    commit

--- ml-models/nlp/text-summarizer/src/extractive_summarizer.py
+++ ml-models/nlp/text-summarizer/src/extractive_summarizer.py
@@ -7,7 +7,9 @@ Combines TF-IDF, TextRank, and sentence position scoring.
 """ TF-IDF and sentence 
     ranking based summarization """	
 
-import numpy as np
+# import numpy as np
 import nltk
 import re
 
+nltk.download('punkt', quiet=True)
+nltk.download('stopwords', quiet=True)
