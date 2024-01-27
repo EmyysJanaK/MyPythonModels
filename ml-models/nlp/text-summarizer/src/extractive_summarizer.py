38da38c289b93114f5f1dbbd7e461e356f267c70
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2024-01-27 13:45:20 +0530

    commit

--- ml-models/nlp/text-summarizer/src/extractive_summarizer.py
+++ ml-models/nlp/text-summarizer/src/extractive_summarizer.py
@@ -1,4 +1,8 @@
 
+"""
+Extractive Text Summarizer using multiple algorithms.
+Combines TF-IDF, TextRank, and sentence position scoring.
+"""
 
 """ TF-IDF and sentence 
     ranking based summarization """	
