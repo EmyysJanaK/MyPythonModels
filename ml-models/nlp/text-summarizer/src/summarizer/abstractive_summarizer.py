383564fe672bc4e5db9c811b906a3cae92349e07
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2024-02-13 10:15:20 +0530

    commit

--- ml-models/nlp/text-summarizer/src/summarizer/abstractive_summarizer.py
+++ ml-models/nlp/text-summarizer/src/summarizer/abstractive_summarizer.py
@@ -2,3 +2,40 @@
 Abstractive Text Summarizer using transformer models.
 Supports multiple pre-trained models and custom fine-tuning.
 """
+
+class AbstractiveSummarizer:
+    """Advanced abstractive text summarizer using transformer models."""
+    
+    # Available models and their configurations
+    MODELS = {
+        'bart-large-cnn': {
+            'tokenizer': BartTokenizer,
+            'model': BartForConditionalGeneration,
+            'max_input': 1024,
+            'description': 'BART model fine-tuned on CNN/DailyMail (good for news)'
+        },
+        't5-base': {
+            'tokenizer': T5Tokenizer,
+            'model': T5ForConditionalGeneration,
+            'max_input': 512,
+            'description': 'T5 base model (versatile, smaller)'
+        },
+        't5-large': {
+            'tokenizer': T5Tokenizer,
+            'model': T5ForConditionalGeneration,
+            'max_input': 512,
+            'description': 'T5 large model (better quality, slower)'
+        },
+        'pegasus-xsum': {
+            'tokenizer': PegasusTokenizer,
+            'model': PegasusForConditionalGeneration,
+            'max_input': 512,
+            'description': 'Pegasus model trained on XSum (good for short summaries)'
+        },
+        'pegasus-cnn_dailymail': {
+            'tokenizer': PegasusTokenizer,
+            'model': PegasusForConditionalGeneration,
+            'max_input': 1024,
+            'description': 'Pegasus model trained on CNN/DailyMail'
+        }
+    }
